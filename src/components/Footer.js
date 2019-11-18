0b0df0ff4a4307368dd57750de3af96c6de814b8
Chris Santamaria <chris@santamaria.me>
2019-11-18 13:26:53 -0500

    Remove previous unused styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Container as MuiContainer, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
 const Container = styled.footer`
   padding: 2rem;
@@ -15,20 +14,6 @@ const ContentContainer = styled(MuiContainer)`
   flex-direction: column;
   align-items: center;
 `;
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     padding: theme.spacing(2),
-//     marginTop: 'auto',
-//     backgroundColor: theme.palette.primary.main,
-//     color: 'white'
-//   },
-//   container: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center'
-//   }
-// }));
 
 export default () => (
   <Container>
